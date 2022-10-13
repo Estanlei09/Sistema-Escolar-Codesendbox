002b5cdf190d542d8e01b8fe1b65b345d6e301d0
Estanlei Oliveira <115293482+Estanlei09@users.noreply.github.com>
2022-10-13 19:30:24 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>SISTEMA ESCOLAR</h1>
+      <h2>SECRETARIA</h2>
+      <h2>ALUNO</h2>
+      <h2>PROFESSOR</h2>
+      <h2>DIRETORIA</h2>
+    </div>
+  );
+}
